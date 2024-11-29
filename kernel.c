d771802c0338ea9e0c268eed1f5354249e9be7e3
elaurentium <credopuke@icloud.com>
2024-11-28 23:38:18 -0300

    arm

--- kernel.c
+++ kernel.c
@@ -1,35 +1,49 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#define FB_WIDTH  80
+#define FB_HEIGHT 25
+#define FB_DEPTH  2
+#define FB_SIZE (FB_WIDTH * FB_HEIGHT * FB_DEPTH)
 
 void main(void) {
     const char *str = "Kernel Runs";
-    /* A memória de vídeo começa no endereço 0xb8000 */
-    char *vidptr = (char*)0xb8000;
+    char *framebuffer;
+    int fb = open("/dev/fb0", O_RDWR);
+    if (fb == -1) {
+        perror("Error opening framebuffer");
+        return;
+    }
+
+    framebuffer = (char*)mmap(NULL, FB_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fb, 0);
+    if (framebuffer == MAP_FAILED) {
+        perror("Error mapping framebuffer");
+        close(fb);
+        return;
+    }
+
     unsigned int i = 0;
     unsigned int j = 0;
-    unsigned int screensize;
 
-    /* Este loop limpa a tela
-     * Existem 25 linhas e 80 colunas; cada elemento ocupa 2 bytes */
-    screensize = 80 * 25 * 2;
-    while (j < screensize) {
-        /* Caracter em branco */
-        vidptr[j] = ' ';
-        /* Byte de atributo */
-        vidptr[j+1] = 0x07;
+    /* Limpar a tela */
+    while (j < FB_SIZE) {
+        framebuffer[j] = ' ';       // Caracter vazio
+        framebuffer[j+1] = 0x07;    // Cor do texto (preto com texto cinza claro)
         j = j + 2;
     }
 
     j = 0;
 
-    /* Este loop escreve a string na memória de vídeo */
+    /* Escrever a string */
     while (str[j] != '\0') {
-        /* O caractere em ASCII */
-        vidptr[i] = str[j];
-        /* Byte de atributo: dá ao caractere fundo preto e texto cinza claro */
-        vidptr[i+1] = 0x07;
+        framebuffer[i] = str[j];   // Caracter em ASCII
+        framebuffer[i+1] = 0x07;   // Cor do texto
         ++j;
         i = i + 2;
     }
 
-    return;
+    munmap(framebuffer, FB_SIZE);
+    close(fb);
 }
